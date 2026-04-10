b3cba96090fea7bdb60f8b6607c30c5ce38b5f8d
ArseniyAbrosimov <senyaabrosimov@mail.ru>
2026-04-11 02:32:08 +0300

    Доделал первый тест
    diff --git c/renamer/src/wasmJsTest/kotlin/ru/hopec/renamer/RenamerPassTest.kt i/renamer/src/wasmJsTest/kotlin/ru/hopec/renamer/RenamerPassTest.kt
    index 2699091..3f2eaa8 100644
    --- c/renamer/src/wasmJsTest/kotlin/ru/hopec/renamer/RenamerPassTest.kt
    +++ i/renamer/src/wasmJsTest/kotlin/ru/hopec/renamer/RenamerPassTest.kt
    @@ -1,12 +1,8 @@
     package ru.hopec.ru.hopec.renamer
    
    -import kotlinx.browser.window
    -import kotlinx.coroutines.GlobalScope
     import kotlinx.coroutines.await
     import kotlinx.coroutines.launch
     import kotlinx.coroutines.test.runTest
    -import org.w3c.fetch.RequestInit
    -import org.w3c.fetch.Response
     import ru.hopec.core.CompilationContext
     import ru.hopec.parser.TreeSitterRepresentation
     import ru.hopec.parser.treesitter.Language
    @@ -16,11 +12,13 @@ import ru.hopec.renamer.AstNode
     import ru.hopec.renamer.RenamedRepresentation
     import ru.hopec.renamer.RenamerPass
     import kotlin.test.Test
    +import kotlin.test.assertEquals
    +import kotlin.test.assertTrue
    
     class RenamerPassTest {
         private suspend fun runRename(source: String): RenamedRepresentation? {
             val context = CompilationContext()
    -        val location = "/tree-sitter-hope.wasm"
    +        val location = "kotlin/tree-sitter-hope.wasm"
    
             Parser.init().await<JsAny?>()
             val parser = Parser()
    @@ -29,36 +27,20 @@ class RenamerPassTest {
             val tree: Tree = parser.parse(source.toJsString(), oldTree = null, options = null)
    
             val treeSitterRep = TreeSitterRepresentation(tree)
    -
    -        println(treeSitterRep.tree.rootNode.type)
             val pass = RenamerPass()
    
             return pass.run(treeSitterRep, context)
         }
    
         @Test
    -    fun testRename() {
    -        test()
    -    }
    -
    -    @Test
    -    fun `test function equation`() =
    +    fun test_function_equation() =
             runTest {
    -            val job = launch {
    -                val code = "--- x <= 42"
    -                val res = runRename(code)
    +                val code = "--- ( x ) <= 42"
    +                val res = runRename(code) ?: return@runTest
    +                assertTrue(res.topLevelNodes.first() is AstNode.FunctionEquation, "Should be parsed as function equation");
    +                val equation = res.topLevelNodes.first() as AstNode.FunctionEquation
    +                assertTrue(equation.pattern is AstNode.IdentPattern, "Should be parsed as ident pattern")
    +                assertTrue(equation.body is AstNode.Ident)
    +                println(equation.body.name)
                 }
    -            job.join()
    -//        assertNotNull(res)
    -//        val script = res.result as? CompilationResult.ScriptResult
    -//        assertNotNull(script, "Should be parsed as a script")
    -//
    -//        val equation = script.statements.first() as AstNode.FunctionEquation
    -//        assertEquals("x", equation.pattern)
    -//        assertTrue(equation.body is AstNode.IntLiteral)
    -        }
     }
    -
    -fun test() {
    -    js("console.log('Доступные файлы:', Object.keys(window.__karma__.files))")
    -}
    \ No newline at end of file

--- renamer/src/wasmJsTest/kotlin/ru/hopec/renamer/RenamerPassTest.kt
+++ renamer/src/wasmJsTest/kotlin/ru/hopec/renamer/RenamerPassTest.kt
@@ -1,12 +1,8 @@
 package ru.hopec.ru.hopec.renamer
 
-import kotlinx.browser.window
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.await
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
-import org.w3c.fetch.RequestInit
-import org.w3c.fetch.Response
 import ru.hopec.core.CompilationContext
 import ru.hopec.parser.TreeSitterRepresentation
 import ru.hopec.parser.treesitter.Language
@@ -16,11 +12,13 @@ import ru.hopec.renamer.AstNode
 import ru.hopec.renamer.RenamedRepresentation
 import ru.hopec.renamer.RenamerPass
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class RenamerPassTest {
     private suspend fun runRename(source: String): RenamedRepresentation? {
         val context = CompilationContext()
-        val location = "/tree-sitter-hope.wasm"
+        val location = "kotlin/tree-sitter-hope.wasm"
 
         Parser.init().await<JsAny?>()
         val parser = Parser()
@@ -29,36 +27,20 @@ class RenamerPassTest {
         val tree: Tree = parser.parse(source.toJsString(), oldTree = null, options = null)
 
         val treeSitterRep = TreeSitterRepresentation(tree)
-
-        println(treeSitterRep.tree.rootNode.type)
         val pass = RenamerPass()
 
         return pass.run(treeSitterRep, context)
     }
 
     @Test
-    fun testRename() {
-        test()
-    }
-
-    @Test
-    fun `test function equation`() =
+    fun test_function_equation() =
         runTest {
-            val job = launch {
-                val code = "--- x <= 42"
-                val res = runRename(code)
+                val code = "--- ( x ) <= 42"
+                val res = runRename(code) ?: return@runTest
+                assertTrue(res.topLevelNodes.first() is AstNode.FunctionEquation, "Should be parsed as function equation");
+                val equation = res.topLevelNodes.first() as AstNode.FunctionEquation
+                assertTrue(equation.pattern is AstNode.IdentPattern, "Should be parsed as ident pattern")
+                assertTrue(equation.body is AstNode.Ident)
+                println(equation.body.name)
             }
-            job.join()
-//        assertNotNull(res)
-//        val script = res.result as? CompilationResult.ScriptResult
-//        assertNotNull(script, "Should be parsed as a script")
-//
-//        val equation = script.statements.first() as AstNode.FunctionEquation
-//        assertEquals("x", equation.pattern)
-//        assertTrue(equation.body is AstNode.IntLiteral)
-        }
-}
-
-fun test() {
-    js("console.log('Доступные файлы:', Object.keys(window.__karma__.files))")
 }
