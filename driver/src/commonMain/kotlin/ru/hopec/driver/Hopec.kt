1b12e6c4b17766d8fc6c71d8843022a8f714751e
Andrey Kashenin <and.kashenin@mail.ru>
2026-05-02 21:10:53 +0300

    added export and fixed ci

--- driver/src/commonMain/kotlin/ru/hopec/driver/Hopec.kt
+++ driver/src/commonMain/kotlin/ru/hopec/driver/Hopec.kt
@@ -28,7 +28,7 @@ class Hopec(
         val watCode =
             """
             (module
-              (func (param i32 i32) (result i32)
+              (func (export "add") (param i32 i32) (result i32)
                 (i32.add (local.get 0) (local.get 1))
               )
               (func (export "main") (result i32)
