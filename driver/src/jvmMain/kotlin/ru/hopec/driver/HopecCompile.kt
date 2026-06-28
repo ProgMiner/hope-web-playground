d7f8e1a18e47a760b0b4752e110e898ae6a589b0
Andrey Kashenin <andkashenin999@gmail.com>
2026-06-28 23:07:18 +0300

    first logic

--- driver/src/jvmMain/kotlin/ru/hopec/driver/HopecCompile.kt
+++ driver/src/jvmMain/kotlin/ru/hopec/driver/HopecCompile.kt
@@ -4,6 +4,7 @@ import com.dylibso.chicory.runtime.Instance
 import com.dylibso.chicory.wasm.Parser
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.path
 import kotlinx.coroutines.runBlocking
 import okio.Buffer
@@ -12,9 +13,16 @@ import ru.hopec.core.isError
 import ru.hopec.parser.treesitter.parseHope
 import java.nio.file.Path
 import kotlin.io.path.readText
+import kotlin.io.path.writeBytes
 
 class HopecCompile : CliktCommand(name = "compile") {
     val input: Path by argument().path(mustExist = true)
+
+    private val output: Path? by option(
+        "-o",
+        "--output",
+        help = "Write the compiled wasm binary to this file instead of executing it",
+    ).path()
 
     override fun run() {
         val tree = runBlocking { parseHope(input.readText()) }
@@ -24,7 +32,13 @@ class HopecCompile : CliktCommand(name = "compile") {
             echo("compilation failed", err = true)
             return
         }
-        val entrance = Instance.builder(Parser.parse(buffer.readByteArray())).build().export("main")
+        val wasmBinary = buffer.readByteArray()
+        if (output != null) {
+            output!!.writeBytes(wasmBinary)
+            echo("wasm written to $output")
+            return
+        }
+        val entrance = Instance.builder(Parser.parse(wasmBinary)).build().export("main")
         echo(entrance.apply(0)[0])
     }
 }
